34df43f012bf499e5ae05efcbb25ce14fca5960d
Yash Chauhan <er.yashjayeshchauhan@gmail.com>
2025-03-06 12:01:01 +0530

    [Updated] code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,68 @@
+import { BlurFade } from "@/components/magicui/blur-fade";
+import Iphone15Pro from "@/components/magicui/iphone-15-pro";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
 import Image from "next/image";
+import backimg from "./back.png";
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex flex-col text-center overflow-hidden">
+      <div className="fixed inset-0 z-[-10] h-screen w-screen bg-white" style={{ background: 'radial-gradient(125% 125% at 50% 10%, #fff 40%, #ee335d 100%)' }}>
+      </div>
+      <div className="overflow-y-auto flex-1 flex flex-col justify-center items-center text-center">
+        <div className="container mx-auto px-6 py-10 md:px-0 md:py-0 mt-14 md:mt-12 max-w-4xl">
+          <h1 className={cn("text-3xl md:text-6xl font-semibold text-slate-800 drop-shadow-md mt-6", font.className)}>
+            <BlurFade delay={0.25} inView>
+              Create
+              <span className="relative inline-block mx-2">
+                <div className="absolute right-0 top-[2px] bg-[#ee335d] w-full h-full rotate-[1.73deg] rounded-[10px] z-[1] px-3"></div>
+                <span className="relative text-white z-[2] px-3">
+                  Video Transcription
+                </span>
+              </span>
+            </BlurFade>
+            <BlurFade delay={0.25 * 2} inView>
+              in
+              <span className="relative inline-block mx-2">
+                <div className="absolute right-0 top-[2px] bg-[#ee335d] w-full h-full rotate-[1.73deg] rounded-[10px] z-[1] px-3"></div>
+                <span className="relative text-white z-[2] px-3">
+                  Seconds
+                </span>
+              </span>
+              using AI
+            </BlurFade>
+          </h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </div>
+
+      <div className="grid gap-x-6 grid-cols-3 justify-center max-w-6xl mx-auto overflow-hidden my-24">
+        <div className="flex justify-center">
+          <Iphone15Pro
+            className="w-full md:max-w-[250px] h-full md:max-h-[500px]"
+            src="https://via.placeholder.com/430x880"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+        <div className="flex justify-center items-center h-full">
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src={backimg}
+            alt="back-img"
+            width={190}
+            height={190}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+        </div>
+        <div className="flex justify-center">
+          <Iphone15Pro
+            className="w-full md:max-w-[250px] h-full md:max-h-[500px]"
+            src="https://via.placeholder.com/430x880"
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </div>
+
+    </main>
   );
 }
